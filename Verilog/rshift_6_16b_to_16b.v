099be073b91b5e0de415791c7bdbbe9fc8da5e0a
Shvetha Senthil Kumar <ssenthilkumar@mario.ece.utexas.edu>
2019-11-30 20:55:51 -0600

    rshift_6_16b_to_16b

---
+++ Verilog/rshift_6_16b_to_16b.v
@@ -0,0 +1,16 @@
+module rshift_6_16b_to_16b(
+Data_In,
+Data_Out);
+
+input [15:0] Data_In;
+output [15:0] Data_Out;
+
+assign Data_Out[15] = Data_In[15]; 
+assign Data_Out[14] = Data_In[15]; 
+assign Data_Out[13] = Data_In[15]; 
+assign Data_Out[12] = Data_In[15]; 
+assign Data_Out[11] = Data_In[15]; 
+
+assign Data_Out[10:0] = Data_In[15:6];
+
+endmodule
