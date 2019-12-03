fbcae8427e8e47687c50723c1ec03d367fef62b5
hibagus <git@bagus.my.id>
2019-12-02 20:58:57 -0600

    fix the mux instantiation

--- Verilog/bypass_32b.v
+++ Verilog/bypass_32b.v
@@ -1,37 +1,71 @@
-module bypass_32b(
-A32,
-TYPESEL,
-R32);
+module bypass_32b (
+  A32,
+  TYPESEL,
+  R32);
 
-input [31:0] A32;
-input [2:0] TYPESEL;
-output [31:0] R32;
-
-wire [15:0] real_a32;
-wire [15:0] imag_a32;
-wire [15:0] real_r32;
-wire [15:0] imag_r32;
-wire [15:0] mux0out;
-wire [15:0] mux1out;
-wire [15:0] mux2out;
-wire [15:0] mux3out;
-wire [15:0] sgninv_0_out;
-wire [15:0] sgninv_1_out;
-
-assign real_a32 = A32[31:16];
-assign imag_a32 = A32[15:0];
-assign real_r32 = mux2out;
-assign imag_r32 = mux3out;
-assign R32[31:16] = real_r32;
-assign R32[15:0] = imag_r32;
-
-mux_2to1_16b mux0(.D1(real_a32), .D2(imag_a32), .Y(mux0out), .S(TYPESEL[2]));
-mux_2to1_16b mux1(.D1(imag_a32), .D2(real_a32), .Y(mux1out), .S(TYPESEL[2]));
-
-sgninv_16b sgninv0(.A16(mux0out), .R16(sgninv_0_out));
-sgninv_16b sgninv1(.A16(mux1out), .R16(sgninv_1_out));
-
-mux_2to1_16b mux2(.D1(mux0out), .D2(sgninv_0_out), .Y(mux2out), .S(TYPESEL[1]));
-mux_2to1_16b mux3(.D1(mux1out), .D2(sgninv_1_out), .Y(mux3out), .S(TYPESEL[0]));
-
+  input  [31:0]  A32;
+  input  [2:0]   TYPESEL;
+  output [31:0]  R32;
+  
+  wire   [31:0]  A32;
+  wire   [2:0]   TYPESEL;
+  wire   [31:0]  R32;
+  
+  wire   [15:0] real_a32;
+  wire   [15:0] imag_a32;
+  wire   [15:0] real_r32;
+  wire   [15:0] imag_r32;
+  wire   [15:0] mux0out;
+  wire   [15:0] mux1out;
+  wire   [15:0] mux2out;
+  wire   [15:0] mux3out;
+  wire   [15:0] sgninv_0_out;
+  wire   [15:0] sgninv_1_out;
+  
+  assign real_a32   = A32[31:16];
+  assign imag_a32   = A32[15:0];
+  assign real_r32   = mux2out;
+  assign imag_r32   = mux3out;
+  assign R32[31:16] = real_r32;
+  assign R32[15:0]  = imag_r32;
+  
+  mux_2_to_1 # (
+    .DATA_WIDTH(16)
+  ) mux_2_to_1_inst0 (
+    .D0(real_a32),
+    .D1(imag_a32),
+    .S(TYPESEL[2]),
+    .Y(mux0out)
+  ); 
+  
+  mux_2_to_1 # (
+    .DATA_WIDTH(16)
+  ) mux_2_to_1_inst0 (
+    .D0(imag_a32),
+    .D1(real_a32),
+    .S(TYPESEL[2]),
+    .Y(mux1out)
+  ); 
+  
+  sgninv_16b sgninv0(.A16(mux0out), .R16(sgninv_0_out));
+  sgninv_16b sgninv1(.A16(mux1out), .R16(sgninv_1_out));
+  
+  mux_2_to_1 # (
+    .DATA_WIDTH(16)
+  ) mux_2_to_1_inst0 (
+    .D0(mux0out),
+    .D1(sgninv_0_out),
+    .S(TYPESEL[1]),
+    .Y(mux2out)
+  ); 
+  
+  mux_2_to_1 # (
+    .DATA_WIDTH(16)
+  ) mux_2_to_1_inst0 (
+    .D0(mux1out),
+    .D1(sgninv_1_out),
+    .S(TYPESEL[0]),
+    .Y(mux3out)
+  ); 
+  
 endmodule
