5c8eb2562a4f1590778c1e211ea4b1e189470d52
hibagus <git@bagus.my.id>
2019-12-04 10:33:50 -0600

    change state machine instantiation

--- Verilog/output_counter.v
+++ Verilog/output_counter.v
@@ -1,55 +1,72 @@
 module output_counter(
-clk,
-rst,
-dataind,
-counter_o,
-datavalid);
-
-input clk;
-input rst;
-input dataind;
-output reg [5:0] counter_o;
-output reg datavalid;
-
-reg currentstate;
-reg idle = 1'b0;
-reg counting = 1'b1;
-
-reg [5:0] counter;
-
-always@(posedge clk)begin
-	counter_o <= counter;
-	if(rst)begin
-		currentstate <= idle;
-	end
-	else begin
-		case (currentstate)
-		0:begin
-			if(dataind)begin
-				currentstate <= counting;
-				counter <= 6'b0;
-				datavalid <= 1'b0;
-			end
-			else begin
-				currentstate <= currentstate;
-				counter <= 6'b0;
-				datavalid <= 1'b0;
-			end
-		end
-		1:begin
-			if(counter == 6'b111110)begin
-				currentstate <= idle;
-				counter <= counter + 1;
-				datavalid <= 1;
-			end
-			else begin
-				currentstate <= counting;
-				counter <= counter + 1;
-				datavalid <= 1;
-			end
-		end
-		endcase
-		currentstate <= currentstate;
-	end
-end
+  clk,
+  rst,
+  dataind,
+  counter_o,
+  datavalid
+  );
+  
+  input        clk;
+  input        rst;
+  input        dataind;
+  output [5:0] counter_o;
+  output       datavalid;
+  
+  wire         clk;
+  wire         rst;
+  wire         dataind;
+  wire   [5:0] counter_o;
+  reg          datavalid;
+  
+  
+  localparam idle     = 1'b0; 
+  localparam counting = 1'b1; 
+  
+  reg currentstate;
+  reg [5:0] counter;
+  
+  assign counter_o = counter;
+  
+  always@(posedge clk)
+    begin
+      if(rst==1'b1)
+        begin
+          currentstate <= idle;
+        end
+      else 
+        begin
+          case (currentstate)
+            idle:
+              begin
+                if(dataind==1'b1)
+                  begin
+                    currentstate <= counting;
+                    counter      <= 6'b0;
+                    datavalid    <= 1'b0;
+                  end
+                else 
+                  begin
+                    currentstate <= currentstate;
+                    counter      <= 6'b0;
+                    datavalid    <= 1'b0;
+                  end
+              end
+            counting:
+              begin
+                if(counter == 6'b111110)
+                  begin
+                    currentstate <= idle;
+                    counter      <= counter + 1;
+                    datavalid    <= 1;
+                  end
+                else 
+                  begin
+                    currentstate <= counting;
+                    counter <= counter + 1;
+                    datavalid <= 1;
+                  end
+              end
+          endcase
+        end
+    end
 endmodule
