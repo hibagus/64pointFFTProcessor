3456b37f745a6af4b37aabcec0bda35338de952e
hibagus <git@bagus.my.id>
2019-12-04 10:34:05 -0600

    reformat module

--- Verilog/real_imaginary_interchange.v
+++ Verilog/real_imaginary_interchange.v
@@ -1,27 +1,44 @@
 module real_imaginary_interchange(
-A32,
-Swap,
-R32);
-
-input [31:0] A32;
-input Swap;
-output [31:0] R32;
-
-wire [15:0] real_a32;
-wire [15:0] imag_a32;
-wire [15:0] real_r32;
-wire [15:0] imag_r32;
-wire [15:0] mux0out;
-wire [15:0] mux1out;
-
-assign real_a32 = A32[31:16];
-assign imag_a32 = A32[15:0];
-assign real_r32 = mux0out;
-assign imag_r32 = mux1out;
-assign R32[31:16] = real_r32;
-assign R32[15:0] = imag_r32;
-
-mux_2to1_16b mux_0(.D1(real_a32), .D2(imag_a32), .Y(mux0out), .S(Swap));
-mux_2to1_16b mux_1(.D1(imag_a32), .D2(real_a32), .Y(mux1out), .S(Swap));
+  A32,
+  Swap,
+  R32
+  );
+  
+  input   [31:0]  A32;
+  input           Swap;
+  output  [31:0]  R32;
+  
+  wire    [15:0]  real_a32;
+  wire    [15:0]  imag_a32;
+  wire    [15:0]  real_r32;
+  wire    [15:0]  imag_r32;
+  wire    [15:0]  mux0out;
+  wire    [15:0]  mux1out;
+  
+  assign real_a32   = A32[31:16];
+  assign imag_a32   = A32[15:0];
+  assign real_r32   = mux0out;
+  assign imag_r32   = mux1out;
+  assign R32[31:16] = real_r32;
+  assign R32[15:0]  = imag_r32;
+  
+  mux_2_to_1 # (
+    .DATA_WIDTH(16)
+  ) mux_2_to_1_inst0 (
+    .D0(real_a32),
+    .D1(imag_a32),
+    .S(Swap),
+    .Y(mux0out)
+  ); 
+  
+  mux_2_to_1 # (
+    .DATA_WIDTH(16)
+  ) mux_2_to_1_inst0 (
+    .D0(imag_a32),
+    .D1(real_a32),
+    .S(Swap),
+    .Y(mux1out)
+  ); 
+  
 
 endmodule
